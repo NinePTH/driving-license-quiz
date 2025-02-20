bf0de58d906575fd3f804f8e4dfb9aa1078b980d
Patihan Prasongphol <patihan70@gmail.com>
2025-02-20 23:24:40 +0700

    Add footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 // import { useState } from 'react'
 // import Quiz from "./components/Quiz";
 import Quiz2 from "./components/Quiz2";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <>
       <Quiz2 />
+      <Footer />
     </>
   );
 }
---
+++ src/components/Footer.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer: React.FC = () => {
+  return (
+    <footer>
+        Made with 💖 by NinePTH
+    </footer>
+  );
+}
+
+export default Footer;
